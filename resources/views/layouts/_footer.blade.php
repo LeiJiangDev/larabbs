0f987d0ed47d689e6e95e4a3af906bf07ad748d7
vagrant <vagrant@homestead>
2018-03-22 14:30:17 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            LeiJiang 模仿练习 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:name@email.com">关于我们</a></p>
+    </div>
+</footer>
