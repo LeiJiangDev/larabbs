e1fa19d447307ef55ca3199c988a1550b43d6e2c
vagrant <vagrant@homestead>
2018-04-05 06:58:52 +0000

    Add queue for translate slug

---
+++ app/Jobs/TranslateSlug.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Handlers\SlugTranslateHandler;
+use App\Models\Topic;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class TranslateSlug implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+
+    protected $topic;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(Topic $topic)
+    {
+        //接收topic模型
+        $this ->topic = $topic;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //请求百度API接口进行翻译
+        $slug = app(SlugTranslateHandler::class) -> translate($this->topic->title);
+
+        //使用DB类更新slug
+        \DB::table('topics')->where('id',$this->topic->id)->update(['slug'=>$slug]);
+    }
+}
