21ab2998ec3d4a572d1becde10ad6b367c9f9035
vagrant <vagrant@homestead>
2018-04-05 03:08:48 +0000

    Auto translate slug

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -24,8 +24,12 @@ class TopicsController extends Controller
 		return view('topics.index', compact('topics'));
 	}
 
-    public function show(Topic $topic)
+    public function show(Request $request,Topic $topic)
     {
+
+        if (! empty($topic->slug) && $topic->slug != $request->slug){
+            return redirect($topic->link(),301);
+        }
         return view('topics.show', compact('topic'));
     }
 
@@ -46,7 +50,7 @@ class TopicsController extends Controller
         $topic->user_id = Auth::id();
         $topic->save();
 
-        return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+        return redirect()->to($topic->link())->with('message', 'Created successfully.');
     }
 
     //编辑话题
@@ -66,7 +70,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->to($topic->link())->with('message', 'Updated successfully.');
 	}
 
 	//删除话题
