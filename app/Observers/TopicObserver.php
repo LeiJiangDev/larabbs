2444c6ec3b62ab79f64017bd62e1114d8975b36c
vagrant <vagrant@homestead>
2018-04-05 01:58:32 +0000

    Prevent XSS injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -21,6 +21,7 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body,'user_topic_body');
         //make_excerpt 自定义辅助函数（bootstrap/helpers.php中添加）
         $topic->excerpt = make_excerpt($topic->body);
 
