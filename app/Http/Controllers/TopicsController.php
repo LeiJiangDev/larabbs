41b5039a6c138ac040b0bd40f521222269c89e26
vagrant <vagrant@homestead>
2018-04-06 15:16:21 +0000

    Sidebar links

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use Auth;
+use App\Models\Link;
 
 class TopicsController extends Controller
 {
@@ -19,15 +20,14 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request,Topic $topic,User $user)
-	{
-		$topics = $topic->WithOrder($request->order)->paginate(20);
+    public function index(Request $request, Topic $topic, User $user, Link $link)
+    {
+        $topics = $topic->withOrder($request->order)->paginate(20);
+        $active_users = $user->getActiveUsers();
+        $links = $link->getAllCached();
 
-		$active_users = $user->getActiveUsers();
-
-
-		return view('topics.index', compact('topics', 'active_users'));
-	}
+        return view('topics.index', compact('topics', 'active_users', 'links'));
+    }
 
     public function show(Request $request,Topic $topic)
     {
