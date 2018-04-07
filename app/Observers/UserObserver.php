cae71b7a29f2bc6842097b6052e5d34375222c6a
vagrant <vagrant@homestead>
2018-04-07 03:51:09 +0000

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,12 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user)
+    {
+        //如果头像为空，保存默认头像
+        if (empty($user->avatar)){
+            $user->avatar = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        }
+    }
 }
